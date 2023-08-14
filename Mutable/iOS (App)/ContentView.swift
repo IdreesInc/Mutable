f3d0ef0015aec21a60d75ce731cb241042359d37
Idrees Hassan <idrees@idreesinc.com>
2023-08-14 19:28:54 -0400

    Switch to SwiftUI scene

---
+++ Mutable/iOS (App)/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Mutable (iOS)
+//
+//  Created by Idrees Hassan on 8/14/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Sexy!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mutable/iOS (App)/MutableApp.swift
@@ -0,0 +1,17 @@
+//
+//  MutableApp.swift
+//  Mutable (iOS)
+//
+//  Created by Idrees Hassan on 8/14/23.
+//
+
+import SwiftUI
+
+@main
+struct MutableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
